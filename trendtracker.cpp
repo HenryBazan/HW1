1e2f2cd6951ba9510ad2efdc01e17da8ea3a9bfb
Henry Bazan <109365110+HenryBazan@users.noreply.github.com>
2023-09-07 15:15:40 +0000

    Working Code

--- trendtracker.cpp
+++ trendtracker.cpp
@@ -1,90 +1,98 @@
 #include "trendtracker.h"
 #include <algorithm>
 
-//initializing a Trendtracker constructor, no definition.
-//Trendtracker::Trendtracker()
-//{}
+// Constructor: Creates a new Trendtracker tracking no hashtags.
+Trendtracker::Trendtracker() {}
 
-void Trendtracker::insert(string ht)
-{
-    if (popularity(ht) == -1) 
-    {
+// Inserts a hashtag (tweeted 0 times) into the Trendtracker.
+void Trendtracker::insert(string ht) {
+    bool found = false;
+    for (Entry &e : E) {
+        if (e.hashtag == ht) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
         Entry entry;
         entry.hashtag = ht;
         entry.pop = 0;
         E.push_back(entry);
     }
-};
+}
 
-int Trendtracker::size()
-{
+// Returns the number of hashtags in the Trendtracker.
+int Trendtracker::size() {
     return E.size();
-};
+}
 
-void Trendtracker::tweeted(string ht)
-{
-    for (auto& entry : E)
-    {
-        if (entry.hashtag == ht)
-        {
-            entry.pop++;
-            return;
+// Adds 1 to the total number times a hashtag has been tweeted.
+void Trendtracker::tweeted(string ht) {
+    for (Entry &e : E) {
+        if (e.hashtag == ht) {
+            e.pop++;
+            break;
         }
     }
-};
+}
 
-int Trendtracker::popularity(string name)
-{
-    for (const auto& entry : E)
-    {
-        if (entry.hashtag == name)
-        {
-            return entry.pop;
+// Returns the number of times a hashtag has been tweeted.
+int Trendtracker::popularity(string name) {
+    for (const Entry &e : E) {
+        if (e.hashtag == name) {
+            return e.pop;
         }
     }
     return -1;
-};
+}
 
-string Trendtracker::top_trend()
-{
-    if(E.empty())
-    {
-        return "";
+// Returns a most-tweeted hashtag.
+string Trendtracker::top_trend() {
+    string top;
+    int maxPop = -1;
+    for (const Entry &e : E) {
+        if (e.pop > maxPop) {
+            maxPop = e.pop;
+            top = e.hashtag;
+        }
     }
-    auto maxEntry = max_element(E.begin(), E.end(), [](const Entry& a, const Entry& b) 
-    {
-        return a.pop < b.pop;
-    });
-    return maxEntry->hashtag;
-};
+    return top;
+}
 
-void Trendtracker::top_three_trends(vector<string> &T)
-{
-    sort(E.begin(), E.end(), [](const Entry& a, const Entry& b)
-    {
-        return a.pop > b.pop;
-    });
-
+// Fills the provided vector with the 3 most-tweeted hashtags.
+void Trendtracker::top_three_trends(vector<string> &T) {
     T.clear();
-    for (int i = 0; i < min(3, static_cast<int>(E.size())); i++)
-    {
-        T.push_back(E[i].hashtag);
-    }
-};
-
-void Trendtracker::remove(string ht)
-{
-    E.erase(remove_if(E.begin(), E.end(), [ht](const Entry& entry)
-    {
-        return entry.hashtag == ht;
-    }), 
-    E.end());
-};
-
-void Trendtracker::top_k_trends(vector<string> &T, int k)
-{
-    sort(E.begin(), E.end(), [](const Entry& a, const Entry& b)
-    {
-        return a.pop > b.pop;
+    vector<Entry> sortedE = E;
+    sort(sortedE.begin(), sortedE.end(), [](const Entry &a, const Entry &b) {
+        return a.pop > b.pop || (a.pop == b.pop && a.hashtag < b.hashtag);
     });
-};
+    int count = min(3, static_cast<int>(sortedE.size()));
+    for (int i = 0; i < count; ++i) {
+        T.push_back(sortedE[i].hashtag);
+    }
+}
+
+// Remove the given hashtag from the trendtracker.
+void Trendtracker::remove(string ht) {
+    auto it = E.begin();
+    while (it != E.end()) {
+        if (it->hashtag == ht) {
+            it = E.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
+// Fills the provided vector with the k most-tweeted hashtags.
+void Trendtracker::top_k_trends(vector<string> &T, int k) 
+{
+    T.clear();
+    vector<Entry> sortedE = E;
+    sort(sortedE.begin(), sortedE.end(), [](const Entry &a, const Entry &b) 
+    {return a.pop > b.pop || (a.pop == b.pop && a.hashtag < b.hashtag);});
+    int count = min(k, static_cast<int>(sortedE.size()));
+    for (int i = 0; i < count; ++i) {
+        T.push_back(sortedE[i].hashtag);
+    }
+}
